597ab0c60fc709e6562311dbbc5be197ef1d31a4
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-08-06 13:15:19 +0800

    openai piyan

---
+++ lib/nlp/core.ts
@@ -0,0 +1,14 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+export const connection_test = async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const chatCompletion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: 'Hello world' }],
+  });
+  console.log(chatCompletion.data.choices[0].message);
+};
---
+++ lib/nlp/test.ts
@@ -0,0 +1,3 @@
+import { connection_test } from './core';
+
+(async () => { connection_test(); })();
